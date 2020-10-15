953ea9d555659150bd016d68c6b0bb32a7bb2e87
pixelkritzel <timo@pixelkritzel.de>
2020-10-15 20:19:22 +0200

    Fixes connection

--- src/api/connection.ts
+++ src/api/connection.ts
@@ -6,27 +6,28 @@ import { store } from './../store/store';
 export const router = express.Router();
 
 export const connection = router.get('/:imageSetId', async (req, res) => {
-  const { imageSetId } = req.params;
-  const imageSet = store.getImageSetById(imageSetId);
-  if (!imageSet) {
-    res.statusCode = 404;
-    res.send();
-    return;
-  }
-  if (imageSet.isDirty) {
-    res.send(imageSet.selectedImage.url);
-  } else {
-    var timeoutId = setTimeout(() => {
-      disposer();
-      res.send();
-    }, 500);
-    var disposer = when(
-      () => imageSet.isDirty,
-      () => {
-        clearTimeout(timeoutId);
-        res.send(imageSet.selectedImage.url);
-        imageSet.setIsDirty(false);
-      }
-    );
-  }
+	const { imageSetId } = req.params;
+	const imageSet = store.getImageSetById(imageSetId);
+	if (!imageSet) {
+		res.statusCode = 404;
+		res.send();
+		return;
+	}
+	if (imageSet.isDirty) {
+		res.send(imageSet.selectedImage.url);
+		imageSet.setIsDirty(false);
+	} else {
+		var timeoutId = setTimeout(() => {
+			disposer();
+			res.send();
+		}, 500);
+		var disposer = when(
+			() => imageSet.isDirty,
+			() => {
+				clearTimeout(timeoutId);
+				res.send(imageSet.selectedImage.url);
+				imageSet.setIsDirty(false);
+			},
+		);
+	}
 });
